5dcdd3d06f6ca8f0c6d1fbd6ba0a2a4920622c80
el_jijuna <pilmee@icloud.com>
2026-04-16 19:03:36 -0500

    feat: implement useBpPackageVersionSize (#49) closes #49

--- packages/bp/src/keys/bpQueryKeys.ts
+++ packages/bp/src/keys/bpQueryKeys.ts
@@ -1,3 +1,4 @@
 export const bpQueryKeys = {
   packageSize: (name: string) => ['bp', 'package', name, 'size'] as const,
+  packageVersionSize: (name: string, version: string) => ['bp', 'package', name, 'size', version] as const,
 } as const;
