e83cb5897b402bf2b5791438bd0ee01aa65b8f31
ElJijuna <pilmee@icloud.com>
2026-06-07 11:36:07 -0500

    chore(@api-hooks/bp): reorder imports to improve consistency across package hooks

--- packages/bp/src/hooks/useBpPackageSize.ts
+++ packages/bp/src/hooks/useBpPackageSize.ts
@@ -1,6 +1,6 @@
-import { useMemo } from 'react';
 import { type UseQueryResult, useQuery } from '@tanstack/react-query';
 import { BundlephobiaClient, type BundleSize } from 'bundlephobia-api-client';
+import { useMemo } from 'react';
 import { bpQueryKeys } from '../keys/bpQueryKeys.js';
 
 export interface UseBpPackageSizeOptions {
