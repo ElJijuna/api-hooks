a9656d6e4481734735d86267be137d678ff85b0b
ElJijuna <pilmee@icloud.com>
2026-07-02 06:49:51 -0500

    feat(@api-hooks/pypi): add queryOptions support to UsePyPIQueryOptions

--- packages/pypi/src/hooks/options.ts
+++ packages/pypi/src/hooks/options.ts
@@ -1,4 +1,8 @@
+import type { QueryOverrides } from '../types.js';
+
 export interface UsePyPIQueryOptions {
   /** Disable the query. Also disabled when required string params are empty. */
   enabled?: boolean;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  queryOptions?: QueryOverrides<any>;
 }
