9451229caa01ba9d2537d3f305c1d1b1709c4a9c
ElJijuna <pilmee@icloud.com>
2026-07-23 22:43:45 -0500

    feat(@api-hooks/bp): remove duplicate export of BpClientContext in index file

--- packages/bp/src/index.ts
+++ packages/bp/src/index.ts
@@ -3,9 +3,9 @@
 // - bundlephobia-api-client (https://www.npmjs.com/package/bundlephobia-api-client)
 // - @tanstack/react-query
 
+export * from './BpClientContext.js';
 export * from './hooks/useBpPackageHistory.js';
 export * from './hooks/useBpPackageSimilar.js';
 export * from './hooks/useBpPackageSize.js';
 export * from './hooks/useBpPackageVersionSize.js';
 export * from './keys/bpQueryKeys.js';
-export * from './BpClientContext.js';
